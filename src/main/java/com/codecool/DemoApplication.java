3636182f770a398f675f140ebb0ca559b79de1a8
Kohányi Róbert <kohanyi.robert@gmail.com>
2021-01-24 07:00:03 +0100

    Allow start without setting env vars, use custom name
    - App now can start even if datasource related things aren't set
    - DEMO_NAME env var influences the title of the app for testing

--- src/main/java/com/codecool/DemoApplication.java
+++ src/main/java/com/codecool/DemoApplication.java
@@ -2,8 +2,15 @@ package com.codecool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
--- src/main/java/com/codecool/IndexController.java
+++ src/main/java/com/codecool/IndexController.java
@@ -1,6 +1,7 @@
 package com.codecool;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
@@ -12,39 +13,64 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class IndexController {
 
-    @Autowired
+    @Value("${spring.datasource.url}")
+    private String dsUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dsUser;
+
+    @Value("${spring.datasource.password}")
+    private String dsPass;
+
+    @Value("${demo.name}")
+    private String demoName;
+
+    @Autowired(required = false)
     private JdbcTemplate jdbcTemplate;
 
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("demoName", demoName);
         model.addAttribute("results", List.of());
         return "index";
     }
 
     @PostMapping("/")
     public String index(@RequestParam(name = "sql") String sql, Model model) {
-        try {
-            if (sql.matches("^\\s*?SELECT.*$")) {
-                var results = jdbcTemplate.queryForList(sql);
-                model.addAttribute("results", results);
-            } else if (sql.matches("^\\s*?(INSERT|UPDATE|DELETE).*$")) {
-                int rows = jdbcTemplate.update(sql);
-                model.addAttribute("alertType", "info");
-                model.addAttribute("alertMessage", "Successful: " + rows + " row(s) affected");
-            } else {
+        model.addAttribute("demoName", demoName);
+        if ("jdbc:postgresql://changeit:5432/changeit".equals(dsUrl)
+                || "changeit".equals(dsUser)
+                || "changeit".equals(dsPass)) {
+            model.addAttribute("alertType", "warning");
+            model.addAttribute("alertMessage", new StringBuilder()
+                    .append("Must set ")
+                    .append("SPRING_DATASOURCE_URL, ")
+                    .append("SPRING_DATASOURCE_USERNAME, ")
+                    .append("SPRING_DATASOURCE_PASSWORD")
+                    .toString());
+        } else {
+            try {
+                if (sql.matches("^\\s*?SELECT.*$")) {
+                    var results = jdbcTemplate.queryForList(sql);
+                    model.addAttribute("results", results);
+                } else if (sql.matches("^\\s*?(INSERT|UPDATE|DELETE).*$")) {
+                    int rows = jdbcTemplate.update(sql);
+                    model.addAttribute("alertType", "info");
+                    model.addAttribute("alertMessage", "Successful: " + rows + " row(s) affected");
+                } else {
+                    model.addAttribute("alertType", "danger");
+                    model.addAttribute("alertMessage", "Unsupported: " + sql);
+                }
+            } catch (DataAccessException ex) {
+                StringWriter sw = new StringWriter();
+                ex.printStackTrace(new PrintWriter(sw));
                 model.addAttribute("alertType", "danger");
-                model.addAttribute("alertMessage", "Unsupported: " + sql);
+                model.addAttribute("alertMessage", "Query error: " + sw.toString());
             }
-        } catch (DataAccessException ex) {
-            StringWriter sw = new StringWriter();
-            ex.printStackTrace(new PrintWriter(sw));
-            model.addAttribute("alertType", "danger");
-            model.addAttribute("alertMessage", "Query error: " + sw.toString());
         }
         return "index";
     }
